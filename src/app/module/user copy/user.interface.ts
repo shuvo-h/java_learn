cf1e6fac71a2bbe21e696d9bd952793a2e3fd725
shuvo Haldar <shuvoh38@gmail.com>
2023-11-22 22:47:41 +0600

    added user schema

---
+++ src/app/module/user copy/user.interface.ts
@@ -0,0 +1,22 @@
+
+type TFullName = {
+    firstName: string,
+    lastName: string,
+}
+type TAddress  = {
+    street: string,
+    city: string,
+    country: string,
+}
+
+export type TUser = {
+    userId: string,
+    username: string,
+    password: string,
+    fullName : TFullName,
+    age: number,
+    email: string,
+    isActive: boolean,
+    hobbies: string[],
+    address : TAddress
+}
